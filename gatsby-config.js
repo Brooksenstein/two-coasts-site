e652d35f1701badee2e660fd5fc8dcc2085c5ac9
Jacob Friedmann <jacobdfriedmann@gmail.com>
2018-04-28 17:36:09 -0700

    First styled site

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,27 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'Two Coasts Games',
+    description: 'A Games Company',
+    keywords: 'games mobile',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
-}
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-styled-components',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: `${__dirname}/src/pages`,
+        name: 'pages',
+      },
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'img',
+        path: `${__dirname}/src/img/`,
+      },
+    },
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
+  ],
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,42 @@
-import React from 'react'
-import Link from 'gatsby-link'
+import React from 'react';
 
-const IndexPage = () => (
-  <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
-  </div>
-)
+import Image from '../components/Image';
+import LinkImage from '../components/LinkImage';
+import Footer from '../components/Footer';
+import Flex from '../components/layout/Flex';
+import Stretch from '../components/layout/Stretch';
 
-export default IndexPage
+const IndexPage = ({ data }) => (
+  <Stretch>
+    <Flex direction="column" justify="space-around" align="center">
+      <Image maxWidth={400} sizes={data.logoImage.sizes} />
+      <h1>We Make Games</h1>
+      <Flex direction="row" justify="space-around" align="center">
+        <LinkImage
+          maxWidth={100}
+          sizes={data.freakiesImage.sizes}
+          href="/freakies/"
+        />
+      </Flex>
+      <Footer />
+    </Flex>
+  </Stretch>
+);
+
+export const query = graphql`
+  query LogoImageQuery {
+    logoImage: imageSharp(id: { regex: "/logo/" }) {
+      sizes(maxWidth: 400) {
+        ...GatsbyImageSharpSizes
+      }
+    }
+
+    freakiesImage: imageSharp(id: { regex: "/freakies/" }) {
+      sizes(maxWidth: 100) {
+        ...GatsbyImageSharpSizes
+      }
+    }
+  }
+`;
+
+export default IndexPage;
